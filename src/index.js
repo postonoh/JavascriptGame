d3a8086fa8b807864d0e71c590644177415a244f
Shelby Poston <postonoh@att.net>
2019-11-14 11:10:59 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import Paddle from "/src/paddle";
+import InputHandler from "./input";
+
+let canvas = document.getElementById("gameScreen");
+
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+
+const GAME_HEIGHT = 600;
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+new InputHandler(paddle);
+
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
